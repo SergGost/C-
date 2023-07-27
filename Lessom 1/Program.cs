3cfcc15c02cb5ca1d9259864cb3070329b5afed2
SergGost <miib86@mail.ru>
2023-07-27 17:15:45 +0300

    Домашка

---
+++ Lessom 1/Program.cs
@@ -0,0 +1,60 @@
+﻿// //sdfsdfdsf
+
+// Console.Write("Ведите число:");
+// int number = int.Parse(Console.ReadLine()!);
+
+// // Математический спопросб
+// int sqr = number * number;
+// Console.WriteLine($"Квадрат числа равен {number} равен {sqr}");
+
+// // Библиотека
+// int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
+// Console.WriteLine($"Квадрат числа {number} равен {sqr1}(библиотека)");
+
+// Console.Write("первое число:");
+// int number = int.Parse(Console.ReadLine()!);
+
+// /*1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+// a = 25; b = 5 -> да
+// a = 2 b = 10 -> нет
+// a = 9; b = -3 -> да
+// a = -3 b = 9 -> нет*/
+
+// Console.Write("Введите первое число: ");
+// int first = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите второе число: ");
+// int second = int.Parse(Console.ReadLine()!);
+// if (first == Convert.ToInt32(Math.Pow(second, 2)))
+// {
+//     Console.WriteLine("Да");
+// }
+// else Console.WriteLine("Нет");
+
+// Console.Write("Введите число: ");
+// int number = int.Parse(Console.ReadLine()!);
+// if(number == 1) Console.WriteLine("Понедельник");
+// if(number == 2) Console.WriteLine("Вторник");
+// if(number == 3) Console.WriteLine("Среда");
+// if(number == 4) Console.WriteLine("Четверг");
+// if(number == 5) Console.WriteLine("Пятница");
+// if(number == 6) Console.WriteLine("Суббота");
+// if(number == 7) Console.WriteLine("Воскресенье");
+// else if(number<1 || number>7) Console.WriteLine("Введен неверный день недели ");
+
+/*. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"*/
+
+
+// Console.WriteLine("Введите число: ");
+// int number = int.Parse(Console.ReadLine());
+
+// int num2 = - number;
+
+// while (num2 <= number) {
+//     Console.Write($"{num2} ");
+//     num2++;
+// }
+
