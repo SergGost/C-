596d26cc34b7701bb27e4c879c22f9cfbac7983b
SergGost <miib86@mail.ru>
2023-07-27 21:48:14 +0300

    Домашка повторно

--- Lessom 1/HomeWork1/Program.cs
+++ Lessom 1/HomeWork1/Program.cs
@@ -68,16 +68,16 @@
 // 8 -> 2, 4, 6, 8
 
 
-Console.Write("Введите N: ");
-int N = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите N: ");
+// int N = int.Parse(Console.ReadLine()!);
 
-int i = 1;
-while(i < N)
-{
-    if(i % 2 == 0 )
-    {
-        Console.Write($"{i} ");
-    }
-    i++;
-}
-Console.WriteLine();
+// int i = 1;
+// while(i < N)
+// {
+//     if(i % 2 == 0 )
+//     {
+//         Console.Write($"{i} ");
+//     }
+//     i++;
+// }
+// Console.WriteLine();
---
+++ lesson 3/Program.cs
@@ -0,0 +1,45 @@
+﻿// Console.Write("Введите день недели: ");
+// int number = int.Parse(Console.ReadLine()!);
+
+
+// switch (number)
+// {
+//     case 6:{
+//         Console.WriteLine("Это суббота!");
+//         break;
+//     }
+//     case 7:{
+//         Console.WriteLine("Это воскресенье!");
+//         break;
+//     }
+//     default:{
+//         Console.WriteLine("Это будний день!");
+//         break;
+//     }
+// }
+
+
+// Console.Write("Введите номер четверти: ");
+// int quarter = int.Parse(Console.ReadLine()!);
+// switch (quarter){
+//     case 1:{
+//         Console.WriteLine("Координаты X и Y больше нуля.");
+//         break;
+//     }
+//     case 2:{
+//         Console.WriteLine("Координаты Y болше нуля, а X меньше нуля");
+//         break;
+//     }    
+//     case 3:{
+//         Console.WriteLine("Координаты X и Y меньше нуля.");
+//         break;
+//     }    
+//     case 4:{
+//         Console.WriteLine("Координаты X болше нуля, а Y меньше нуля");
+//         break;
+//     }
+//     default:{
+//         Console.WriteLine("Номер четверти не корректен.");
+//         break;
+//     }
+// }
